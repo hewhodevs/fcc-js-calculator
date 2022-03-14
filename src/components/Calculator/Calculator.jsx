ba734c76e5442dcf4329343c7f0c3f08756a47e6
hewhodevs <hewhodevs@outlook.com>
2022-03-14 16:10:03 +1030

    display now updates with number inputs

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -4,13 +4,28 @@ class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      display: 0,
+      display: "0",
     }
     this.clearDisplay = this.clearDisplay.bind(this);
+    this.showInput = this.showInput.bind(this);
   }
 
+  // clear the display
   clearDisplay() {
-    this.setState({display: 0});
+    this.setState({display: "0"});
+  }
+
+  // Show numbers as input on the display. prevent leading zero's on display
+  showInput(inputNumString) {
+    let currentDisplayValue = this.state.display;
+    if(currentDisplayValue === "0") {
+      // dont allow leading zero's
+      this.setState({display: inputNumString})
+    } else {
+      this.setState((prevState) => ({
+        display: prevState.display + inputNumString
+      }));
+    }
   }
 
 
@@ -27,20 +42,22 @@ class Calculator extends React.Component {
         <button id="divide">/</button>
 
         <button id="clear" onClick={this.clearDisplay}>C</button>
+
         <button id="decimal">.</button>
         <button id="equals">=</button>
 
+
+        <button id="zero" onClick={() => {this.showInput("0")}}>0</button>
+        <button id="one" onClick={() => {this.showInput("1")}}>1</button>
+        <button id="two" onClick={() => {this.showInput("2")}}>2</button>
+        <button id="three" onClick={() => {this.showInput("3")}}>3</button>
+        <button id="four" onClick={() => {this.showInput("4")}}>4</button>
+        <button id="five" onClick={() => {this.showInput("5")}}>5</button>
+        <button id="six" onClick={() => {this.showInput("6")}}>6</button>
+        <button id="seven" onClick={() => {this.showInput("7")}}>7</button>
+        <button id="eight" onClick={() => {this.showInput("8")}}>8</button>
+        <button id="nine" onClick={() => {this.showInput("9")}}>9</button>
         
-        <button id="zero">0</button>
-        <button id="one">1</button>
-        <button id="two">2</button>
-        <button id="three">3</button>
-        <button id="four">4</button>
-        <button id="five">5</button>
-        <button id="six">6</button>
-        <button id="seven">7</button>
-        <button id="eight">8</button>
-        <button id="nine">9</button>
       </div>
     );
   }
