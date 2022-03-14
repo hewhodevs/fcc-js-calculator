2e9e662e9685719bd596ecaf12e291870327ca63
hewhodevs <hewhodevs@outlook.com>
2022-03-14 21:12:20 +1030

    basic calculator appearence styles

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Calculator.css"
 
 class Calculator extends React.Component {
   constructor(props) {
@@ -34,29 +35,42 @@ class Calculator extends React.Component {
     return (
       <div className="Calculator">
 
-        <span id="display">{this.state.display}</span>
+        <div className="display-container">
+          <button id="clear" onClick={this.clearDisplay}>C</button>
+          <span id="display">{this.state.display}</span>
+        </div>
 
-        <button id="add">+</button>
-        <button id="subtract">-</button>
-        <button id="multiply">x</button>
-        <button id="divide">/</button>
-
-        <button id="clear" onClick={this.clearDisplay}>C</button>
-
-        <button id="decimal">.</button>
-        <button id="equals">=</button>
-
-
-        <button id="zero" onClick={() => {this.showInput("0")}}>0</button>
-        <button id="one" onClick={() => {this.showInput("1")}}>1</button>
-        <button id="two" onClick={() => {this.showInput("2")}}>2</button>
-        <button id="three" onClick={() => {this.showInput("3")}}>3</button>
-        <button id="four" onClick={() => {this.showInput("4")}}>4</button>
-        <button id="five" onClick={() => {this.showInput("5")}}>5</button>
-        <button id="six" onClick={() => {this.showInput("6")}}>6</button>
-        <button id="seven" onClick={() => {this.showInput("7")}}>7</button>
-        <button id="eight" onClick={() => {this.showInput("8")}}>8</button>
-        <button id="nine" onClick={() => {this.showInput("9")}}>9</button>
+        <div className="pad">
+          <div className="operations">
+            <button id="add">+</button>
+            <button id="subtract">-</button>
+            <button id="multiply">x</button>
+            <button id="divide">/</button>
+            <button id="equals">=</button>
+          </div>
+          
+          <div className="numpad">
+            <div className="row">
+              <button id="seven" onClick={() => {this.showInput("7")}}>7</button>
+              <button id="eight" onClick={() => {this.showInput("8")}}>8</button>
+              <button id="nine" onClick={() => {this.showInput("9")}}>9</button>
+            </div>
+            <div className="row">
+              <button id="four" onClick={() => {this.showInput("4")}}>4</button>
+              <button id="five" onClick={() => {this.showInput("5")}}>5</button>
+              <button id="six" onClick={() => {this.showInput("6")}}>6</button>
+            </div>
+            <div className="row">
+              <button id="one" onClick={() => {this.showInput("1")}}>1</button>
+              <button id="two" onClick={() => {this.showInput("2")}}>2</button>
+              <button id="three" onClick={() => {this.showInput("3")}}>3</button>
+            </div>
+            <div className="row">
+              <button id="zero" onClick={() => {this.showInput("0")}}>0</button>
+              <button id="decimal">.</button>
+            </div>
+          </div>
+        </div>
         
       </div>
     );
