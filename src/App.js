a46e28d9a89d68a39f2ab22de64e6cae4f880ffe
hewhodevs <hewhodevs@outlook.com>
2022-03-15 15:42:39 +1030

    added footer with repo link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
       <Calculator />
+      <footer>
+        <p>Created by Joshua Kelly</p>
+        <p>Repository available on <a href="https://github.com/hewhodevs/fcc-js-calculator">GitHub</a></p>
+      </footer>
     </div>
   );
 }
